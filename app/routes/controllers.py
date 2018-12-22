236247da8661dddf96f194de8cf7c104f1f8bbcd
Clay McGinnis <mcginnis@claymav.com>
2018-12-21 20:26:54 -0600

    Cleaning

--- app/routes/controllers.py
+++ app/routes/controllers.py
@@ -1,17 +1,12 @@
-# Import flask dependencies
 from flask import (Blueprint, flash, g, redirect, render_template, request,
                    session, url_for)
 
-# Import the database object from the main app module
 from app import db
-# Import module models (i.e. User)
 from app.models import User
 
-# Define the blueprint: 'auth', set its url prefix: app.url/auth
 routes = Blueprint('routes', __name__, url_prefix='/')
 
 
-# Set the route and accepted methods
 @routes.route('/', methods=['GET'])
 def home():
     stuff = [x.name for x in User.select()]
