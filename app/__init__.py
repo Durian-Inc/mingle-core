cc27405b747c573fd6951e18dee3085ca52d4690
Clay McGinnis <mcginnis@claymav.com>
2018-12-27 00:01:26 -0600

    Remove unused imports in app/__init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,3 @@
-import os
-
-from dotenv import load_dotenv
-from flask import Flask, render_template
-from playhouse.postgres_ext import PostgresqlExtDatabase
-
 from app.serve import app
 from app.users.controllers import users
 from app.chats.controllers import chats
